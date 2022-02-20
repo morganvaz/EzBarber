ea1c03cdf1246a7c7f881ac49f43c5ec4cd52630
Morgan <81557708+morganvaz@users.noreply.github.com>
2022-02-20 19:05:19 +0200

    commented out more unused code that will be used on later updates.

--- app/src/main/java/dev/morganv/ezbarber/Activities/DayActivity.java
+++ app/src/main/java/dev/morganv/ezbarber/Activities/DayActivity.java
@@ -9,19 +9,25 @@ import com.google.android.material.textview.MaterialTextView;
 
 import dev.morganv.ezbarber.R;
 
+//        ####################################################################
+//        ####################################################################
+//        ######################### CURRENTLY UNUSED #########################
+//        ####################################################################
+//        ####################################################################
+
 public class DayActivity extends AppCompatActivity {
 
-    MaterialTextView titleDate;
-    MaterialTextView selectedHour;
-    MaterialButton confirm;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_day);
-
-        titleDate = findViewById(R.id.day_LBL_title);
-        selectedHour = findViewById(R.id.day_LBL_selectedhour);
-        confirm = findViewById(R.id.day_BTN_confirm);
-    }
+//    MaterialTextView titleDate;
+//    MaterialTextView selectedHour;
+//    MaterialButton confirm;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_day);
+//
+//        titleDate = findViewById(R.id.day_LBL_title);
+//        selectedHour = findViewById(R.id.day_LBL_selectedhour);
+//        confirm = findViewById(R.id.day_BTN_confirm);
+//    }
 }
--- app/src/main/java/dev/morganv/ezbarber/Activities/SecondActivity.java
+++ app/src/main/java/dev/morganv/ezbarber/Activities/SecondActivity.java
@@ -22,25 +22,25 @@ import dev.morganv.ezbarber.R;
  */
 
 public class SecondActivity extends AppCompatActivity {
-
-    private AppBarConfiguration mAppBarConfiguration;
-    private NavigationView navigationView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
-
-        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_myAppointments, R.id.nav_calendar, R.id.nav_aboutUs,R.id.nav_settings,R.id.nav_logout)
-                .setOpenableLayout(drawer)
-                .build();
-        final NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
+//
+//    private AppBarConfiguration mAppBarConfiguration;
+//    private NavigationView navigationView;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_second);
+//
+//        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+//        navigationView = findViewById(R.id.nav_view);
+//        // Passing each menu ID as a set of Ids because each
+//        // menu should be considered as top level destinations.
+//        mAppBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.nav_myAppointments, R.id.nav_calendar, R.id.nav_aboutUs,R.id.nav_settings,R.id.nav_logout)
+//                .setOpenableLayout(drawer)
+//                .build();
+//        final NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+//        NavigationUI.setupWithNavController(navigationView, navController);
+//    }
 }
